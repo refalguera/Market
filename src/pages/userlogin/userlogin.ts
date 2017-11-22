3319035d5ee25d2647e70d3632d3df0615838f00
Vinicius Ferraz <vini.kyren@gmail.com>
2017-11-22 14:36:38 -0200

    Tela de Busca V0 adicionada

--- src/pages/userlogin/userlogin.ts
+++ src/pages/userlogin/userlogin.ts
@@ -15,11 +15,54 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UserloginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	SearchQuery: string = '';	// Variável para pesquisa
+	items: string[];			// Variável para armazenar os resultados
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserloginPage');
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+	}
+
+
+
+	// Inicialização dos itens
+	initializeItems() {	
+		//Adicionar aqui todos os itens de pesquisa. Lojas e Produtos
+		this.items = [ 
+			'batman',
+			'superman',
+			'betman',
+			'soperman',
+			'bitman',
+			'siperman',
+			'botman',
+			'seperman',
+			'butman',
+			'saperman'
+		]; 
+	}
+	
+	// Busca
+	getItems(ev: any){
+
+		// Pega os itens inicializados.
+		this.initializeItems();
+
+		// Seta a pesquisa na variável 
+		let val = ev.target.value;
+		
+		/* Filtra de acordo com o que está no searchbar
+		O processo funciona de forma automatica enquanto está sendo digitado.
+		Selecionando aquilo que *CONTÉM* o que atualmente está no SearchBar	*/		
+		if (val && val.trim() != ''){
+			this.items = this.items.filter((item) => {
+				return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+			})
+		}
+
+	}	
+
+  	ionViewDidLoad() {
+    	console.log('ionViewDidLoad UserloginPage');
+  	}
 
 }
