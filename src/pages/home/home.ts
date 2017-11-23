d8aff0b0dd6a62b37c39529c83c3df93c3ea1ccd
Vinicius Ferraz <vini.kyren@gmail.com>
2017-11-23 16:21:12 -0200

    atualização com Paccola - Camera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { CadastroPage } from '../cadastro/cadastro';
 import { UserloginPage } from '../userlogin/userlogin';
 import { EmpresaPage } from '../empresa/empresa';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
