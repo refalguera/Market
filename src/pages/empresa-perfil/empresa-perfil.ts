bc49dcf59594ae5e9336e15c658b00fbed7367f1
Vinicius Ferraz <vini.kyren@gmail.com>
2017-11-23 21:46:54 -0200

    pagina de perfil de cliente incluida

--- src/pages/empresa-perfil/empresa-perfil.ts
+++ src/pages/empresa-perfil/empresa-perfil.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
 
 /**
  * Generated class for the EmpresaPerfilPage page.
