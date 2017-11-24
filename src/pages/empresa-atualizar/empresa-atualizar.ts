c6a7369d4cfae4c4919315e11e3528e278af3a89
Joao Pedro Paccola <paccola31@gmail.com>
2017-11-24 12:23:45 -0200

    finalizado perfil da empresa, lista de produtos da empresa, correção de alguns bugs

--- src/pages/empresa-atualizar/empresa-atualizar.ts
+++ src/pages/empresa-atualizar/empresa-atualizar.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
 
 /**
  * Generated class for the EmpresaAtualizarPage page.
@@ -23,9 +24,21 @@ export class EmpresaAtualizarPage {
   dono:any;
   user:any;
   pass:any;
+  base64Image:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alerCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alerCtrl: AlertController, public camera:Camera) {
   }
+
+  accessGalleryAtt(){
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+      destinationType: this.camera.DestinationType.DATA_URL
+    }).then((imageData) => {
+      this.base64Image = 'data:image/jpeg;base64,'+imageData;
+    }, (err) => {
+    console.log(err);
+  });
+}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EmpresaAtualizarPage');
--- src/pages/empresa-perfil/empresa-perfil.ts
+++ src/pages/empresa-perfil/empresa-perfil.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-<<<<<<< HEAD
 import { ParallaxDirective } from '../../directives/parallax/parallax';
 import { EmpresaProdutosPage } from '../empresa-produtos/empresa-produtos';
 import { EmpresaNovoProdutoPage } from '../empresa-novo-produto/empresa-novo-produto';
 import { EmpresaAtualizarPage } from '../empresa-atualizar/empresa-atualizar';
 import { HomePage } from '../home/home';
-
-=======
 import { Camera } from '@ionic-native/camera';
->>>>>>> 3a1fd2f69b83143eec131b0da2c3ef7e42ec439a
 
 /**
  * Generated class for the EmpresaPerfilPage page.
